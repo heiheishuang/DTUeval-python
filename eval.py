ffbf6fc369ee5cec03f5b9b7247827d9f282d3e0
heiheishuang <yingsh.zou@gmail.com>
2022-10-08 21:53:51 +0800

    fix result output

--- eval.py
+++ eval.py
@@ -8,22 +8,25 @@ from scipy.io import loadmat
 import multiprocessing as mp
 import argparse
 
+
 def sample_single_tri(input_):
     n1, n2, v1, v2, tri_vert = input_
-    c = np.mgrid[:n1+1, :n2+1]
+    c = np.mgrid[:n1 + 1, :n2 + 1]
     c += 0.5
     c[0] /= max(n1, 1e-7)
     c[1] /= max(n2, 1e-7)
-    c = np.transpose(c, (1,2,0))
+    c = np.transpose(c, (1, 2, 0))
     k = c[c.sum(axis=-1) < 1]  # m2
-    q = v1 * k[:,:1] + v2 * k[:,1:] + tri_vert
+    q = v1 * k[:, :1] + v2 * k[:, 1:] + tri_vert
     return q
+
 
 def write_vis_pcd(file, points, colors):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     pcd.colors = o3d.utility.Vector3dVector(colors)
     o3d.io.write_point_cloud(file, pcd)
+
 
 if __name__ == '__main__':
     mp.freeze_support()
@@ -46,10 +49,11 @@ if __name__ == '__main__':
 
     ply_list = os.listdir(args.data_output)
 
+    f_result = open(args.data_output + "/result.txt", "w")
+
     for file_name in ply_list:
         if os.path.isdir(os.path.join(args.data_output, file_name)):
             ply_list.remove(file_name)
-
 
     for file_name in ply_list:
 
@@ -66,12 +70,12 @@ if __name__ == '__main__':
 
             pbar.update(1)
             pbar.set_description('sample pcd from mesh')
-            v1 = tri_vert[:,1] - tri_vert[:,0]
-            v2 = tri_vert[:,2] - tri_vert[:,0]
+            v1 = tri_vert[:, 1] - tri_vert[:, 0]
+            v2 = tri_vert[:, 2] - tri_vert[:, 0]
             l1 = np.linalg.norm(v1, axis=-1, keepdims=True)
             l2 = np.linalg.norm(v2, axis=-1, keepdims=True)
             area2 = np.linalg.norm(np.cross(v1, v2), axis=-1, keepdims=True)
-            non_zero_area = (area2 > 0)[:,0]
+            non_zero_area = (area2 > 0)[:, 0]
             l1, l2, area2, v1, v2, tri_vert = [
                 arr[non_zero_area] for arr in [l1, l2, area2, v1, v2, tri_vert]
             ]
@@ -80,7 +84,9 @@ if __name__ == '__main__':
             n2 = np.floor(l2 / thr)
 
             with mp.Pool() as mp_pool:
-                new_pts = mp_pool.map(sample_single_tri, ((n1[i,0], n2[i,0], v1[i:i+1], v2[i:i+1], tri_vert[i:i+1,0]) for i in range(len(n1))), chunksize=1024)
+                new_pts = mp_pool.map(sample_single_tri,
+                                      ((n1[i, 0], n2[i, 0], v1[i:i + 1], v2[i:i + 1], tri_vert[i:i + 1, 0]) for i in
+                                       range(len(n1))), chunksize=1024)
 
             new_pts = np.concatenate(new_pts, axis=0)
             data_pcd = np.concatenate([vertices, new_pts], axis=0)
@@ -115,13 +121,13 @@ if __name__ == '__main__':
         BB = BB.astype(np.float32)
 
         patch = args.patch_size
-        inbound = ((data_down >= BB[:1]-patch) & (data_down < BB[1:]+patch*2)).sum(axis=-1) ==3
+        inbound = ((data_down >= BB[:1] - patch) & (data_down < BB[1:] + patch * 2)).sum(axis=-1) == 3
         data_in = data_down[inbound]
 
         data_grid = np.around((data_in - BB[:1]) / Res).astype(np.int32)
-        grid_inbound = ((data_grid >= 0) & (data_grid < np.expand_dims(ObsMask.shape, 0))).sum(axis=-1) ==3
+        grid_inbound = ((data_grid >= 0) & (data_grid < np.expand_dims(ObsMask.shape, 0))).sum(axis=-1) == 3
         data_grid_in = data_grid[grid_inbound]
-        in_obs = ObsMask[data_grid_in[:,0], data_grid_in[:,1], data_grid_in[:,2]].astype(np.bool_)
+        in_obs = ObsMask[data_grid_in[:, 0], data_grid_in[:, 1], data_grid_in[:, 2]].astype(np.bool_)
         data_in_obs = data_in[grid_inbound][in_obs]
 
         pbar.update(1)
@@ -140,8 +146,8 @@ if __name__ == '__main__':
         pbar.set_description('compute stl2data')
         ground_plane = loadmat(f'{args.dataset_dir}/ObsMask/Plane{int(scan)}.mat')['P']
 
-        stl_hom = np.concatenate([stl, np.ones_like(stl[:,:1])], -1)
-        above = (ground_plane.reshape((1,4)) * stl_hom).sum(-1) > 0
+        stl_hom = np.concatenate([stl, np.ones_like(stl[:, :1])], -1)
+        above = (ground_plane.reshape((1, 4)) * stl_hom).sum(-1) > 0
         stl_above = stl[above]
 
         nn_engine.fit(data_in)
@@ -151,23 +157,23 @@ if __name__ == '__main__':
         pbar.update(1)
         pbar.set_description('visualize error')
         vis_dist = args.visualize_threshold
-        R = np.array([[1,0,0]], dtype=np.float64)
-        G = np.array([[0,1,0]], dtype=np.float64)
-        B = np.array([[0,0,1]], dtype=np.float64)
-        W = np.array([[1,1,1]], dtype=np.float64)
+        R = np.array([[1, 0, 0]], dtype=np.float64)
+        G = np.array([[0, 1, 0]], dtype=np.float64)
+        B = np.array([[0, 0, 1]], dtype=np.float64)
+        W = np.array([[1, 1, 1]], dtype=np.float64)
         data_color = np.tile(B, (data_down.shape[0], 1))
         data_alpha = dist_d2s.clip(max=vis_dist) / vis_dist
-        data_color[ np.where(inbound)[0][grid_inbound][in_obs] ] = R * data_alpha + W * (1-data_alpha)
-        data_color[ np.where(inbound)[0][grid_inbound][in_obs][dist_d2s[:,0] >= max_dist] ] = G
+        data_color[np.where(inbound)[0][grid_inbound][in_obs]] = R * data_alpha + W * (1 - data_alpha)
+        data_color[np.where(inbound)[0][grid_inbound][in_obs][dist_d2s[:, 0] >= max_dist]] = G
         write_vis_pcd(f'{args.data_output}/scan{int(scan)}/vis_{int(scan):03}_d2s.ply', data_down, data_color)
         stl_color = np.tile(B, (stl.shape[0], 1))
         stl_alpha = dist_s2d.clip(max=vis_dist) / vis_dist
-        stl_color[ np.where(above)[0] ] = R * stl_alpha + W * (1-stl_alpha)
-        stl_color[ np.where(above)[0][dist_s2d[:,0] >= max_dist] ] = G
+        stl_color[np.where(above)[0]] = R * stl_alpha + W * (1 - stl_alpha)
+        stl_color[np.where(above)[0][dist_s2d[:, 0] >= max_dist]] = G
         write_vis_pcd(f'{args.data_output}/scan{int(scan)}/vis_{int(scan):03}_s2d.ply', stl, stl_color)
 
         pbar.update(1)
         pbar.set_description('done')
         pbar.close()
         over_all = (mean_d2s + mean_s2d) / 2
-        print(scan, " acc: ", mean_d2s, ", comp: ", mean_s2d, "over: ", over_all)
+        f_result.write(f'{scan}, " acc: ", {mean_d2s}, ", comp: ", {mean_s2d}, "over: ", {over_all}')
