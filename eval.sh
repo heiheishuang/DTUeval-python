e56db09778d42e84f208bf746d357be4becc73fd
heiheishuang <yingsh.zou@gmail.com>
2022-10-08 19:38:32 +0800

    eval.sh

---
+++ eval.sh
@@ -0,0 +1,27 @@
+OUTPUT_DIR="/data/zys_data/trans_origin_b4_15"
+DATASET_DIR="/data/zys_data/dtu_eval/SampleSet/MVS_Data"
+
+python eval.py --data ${OUTPUT_DIR}/mvsnet001_l3.ply --scan   1 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet001_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet004_l3.ply --scan   4 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet004_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet009_l3.ply --scan   9 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet009_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet010_l3.ply --scan  10 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet010_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet011_l3.ply --scan  11 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet011_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet012_l3.ply --scan  12 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet012_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet013_l3.ply --scan  13 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet013_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet015_l3.ply --scan  15 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet015_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet023_l3.ply --scan  23 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet023_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet024_l3.ply --scan  24 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet024_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet029_l3.ply --scan  29 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet029_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet032_l3.ply --scan  32 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet032_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet033_l3.ply --scan  33 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet033_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet034_l3.ply --scan  34 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet034_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet048_l3.ply --scan  48 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet048_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet049_l3.ply --scan  49 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet049_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet062_l3.ply --scan  62 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet062_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet075_l3.ply --scan  75 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet075_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet077_l3.ply --scan  77 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet077_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet110_l3.ply --scan 110 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet110_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet114_l3.ply --scan 114 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet114_l3
+python eval.py --data ${OUTPUT_DIR}/mvsnet118_l3.ply --scan 118 --mode pcd --dataset_dir ${DATASET_DIR} --vis_out_dir ${OUTPUT_DIR}/mvsnet118_l3
+
+
